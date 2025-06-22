6f49e30bbfd214d9741fcfc1f80891682bde2219
nikhilshakya07 <nikhilshakya1308@gmail.com>
2025-06-22 10:00:00 +0530

    added footer

---
+++ frontend/src/components/common/Footer.jsx
@@ -0,0 +1,7 @@
+const Footer = () => (
+  <footer className="text-center py-4 bg-gray-100 text-sm text-gray-500">
+    © {new Date().getFullYear()} BeYourLoop. All rights reserved.
+  </footer>
+);
+
+export default Footer;
