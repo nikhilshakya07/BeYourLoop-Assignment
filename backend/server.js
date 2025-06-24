22cce69e71071bbc85c16882c81395fd3fac1623
nikhilshakya07 <nikhilshakya1308@gmail.com>
2025-06-24 10:00:00 +0530

    Added root route and added meaningful comments

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,13 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 
+//load env vars
 dotenv.config();
+
+//init app
 const app = express();
+
+//connect DB
 connectDB();
 
 // Middleware
@@ -14,8 +19,13 @@ app.use(express.json());
 // Routes
 app.use('/api/auth', require('./routes/authRoutes'));
 
-const PORT = process.env.PORT || 5000;
+// Root route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 
+//Start server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running at: http://localhost:${PORT}`);
 });
